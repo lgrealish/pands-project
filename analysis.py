33c64e6d758bea545b8d1204164176ad208bc64f
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-04-27 20:44:49 +0100

    add hist code

--- analysis.py
+++ analysis.py
@@ -1,27 +1,49 @@
-import numpy as py
+# analysis.py
+# Author: Linda Grealish
+# This program reads in data from the iris.csv file and produces various analysis
+
+
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import sys
 
 
-# variable ids stands for iris flower dataset
+# variable ifds stands for iris flower dataset
 # with read.csv() we are reading the .csv file and storing it into ifds variable for further use and manipulation
 # with "index_col='Id'" I am eliminating the Id column as it is unnecessary in this case
 # reference for index_col: https://realpython.com/python-csv/
 
-ids = pd.read_csv('iris.csv', index_col='Id')
-
-print(ids)
-print (ids.describe())
-print (ids.info())
+ifds = pd.read_csv('iris.csv', index_col='Id')
+'''
+print(ifds)
+print (ifds.describe())
+print (ifds.info())
 
 
 #https://medium.com/@avulurivenkatasaireddy/exploratory-data-analysis-of-iris-data-set-using-python-823e54110d2d
-print (ids["Species"].value_counts())
+print (ifds["Species"].value_counts())
 
-ids.hist()
+ifds.hist()
 plt.show()
 
 sns.set()
-sns.pairplot(ids, hue="Species")
+sns.pairplot(ifds, hue="Species")
 plt.show()
+'''
+def sepal_length_hist():
+    # https://stackoverflow.com/questions/332289/how-do-you-change-the-size-of-figures-drawn-with-matplotlib
+    plt.figure(figsize = (9,9))
+    
+    
+    # https://cmdlinetips.com/2019/02/how-to-make-histogram-in-python-with-pandas-and-seaborn/
+    sns.distplot(iris_s["SepalLengthCm"],  kde = False, label = "Iris setosa", color = "deeppink", )
+    sns.distplot(iris_vers["SepalLengthCm"],  kde = False, label = "Iris versicolor", color = "mediumorchid")
+    sns.distplot(iris_virg["SepalLengthCm"],  kde = False, label = "Iris virginica", color = "navy")
+    plt.title("Sepal length in cm", size = 20)
+    plt.xlabel("")
+    plt.ylabel("Frequency", size = 16)
+    plt.legend()
+    #plt.savefig("Sepal-lenght.png")
+    plt.show()
