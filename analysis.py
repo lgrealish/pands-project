eee540fdf25e93969ca230bf28786de4f97c7b11
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-05-04 16:26:39 +0100

    add extra information to output variable file

--- analysis.py
+++ analysis.py
@@ -1,13 +1,11 @@
 # analysis.py
 # Author: Linda Grealish
-# This program reads in data from the iris.csv file and produces various analysis
+# This program reads in data from the iris.csv file and produces various analysis, plot and summary outputs
 
 # https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
 # https://gist.github.com/accessnash/7c255c0cfc12d2725ac79f6710ba19a3
 # https://notebook.community/xR86/ml-stuff/kaggle/iris/Simple%20analysis%20of%20Iris%20dataset
 # https://realpython.com/pandas-plot-python/
-
-
 
 # import the modules needed 
 import numpy as np
@@ -32,32 +30,49 @@ iris_vers = ifds[ifds.Species == "Iris-versicolor"]
 iris_virg = ifds[ifds.Species == "Iris-virginica"]
 
 # code for outputting variables summary data to .txt file
+# https://www.geeksforgeeks.org/sys-stdout-write-in-python/
 def summary_output():
     sys.stdout = open ("output_summary_variabe.txt","w")
     print ("\n")
     print ("==============================================================================")
     print ("Details of the whole dataset:")
-    print (ifds)
     print ("\n")
+    print (ifds)
     print ("==============================================================================")
     print ("Summary Statistics for the Iris Flower Dataset Dataframe")
+    print ("\n")
     print (ifds.describe())
     print ("\n")
     print ("==============================================================================")
+    print ("A concise summary of the Iris Flower Dataset Dataframe")
+    print ("\n")
     print (ifds.info())
     print ("\n")
     print ("==============================================================================")
     print ("The number of distinct classes or species types of iris in the dataframe")
+    print ("\n")
     print (ifds.nunique)
     print ("\n")
     print ("==============================================================================")
-    print ("A summary of the mean and median for sepal length, sepal width, petal length and petal width for each of the three species")
+    print("Number of occurances of each of the species:")
+    print ("\n")
+    # https://medium.com/@avulurivenkatasaireddy/exploratory-data-analysis-of-iris-data-set-using-python-823e54110d2d
+    print (ifds["Species"].value_counts())
+    print ("\n")
+    print ("In percentile:")
+    print("\n")
+    # https://towardsdatascience.com/getting-more-value-from-the-pandas-value-counts-aa17230907a6
+    print (((ifds["Species"].value_counts(normalize=True))*100))
+    print ("\n")
+    print ("==============================================================================")
+    # https://www.tutorialspoint.com/exploratory-data-analysis-on-iris-dataset
+    print ("A summary of the mean and median for sepal length, sepal width, petal length \n and petal width for each of the three species")
+    print ("\n")
     print(ifds.groupby('Species').agg([np.mean, np.median]))
     print ("\n")
     print ("==============================================================================")
-    print ("The ")
     sys.stdout.close()    
-
+'''
 # https://realpython.com/pandas-groupby/#pandas-groupby-putting-it-all-together
 # https://www.tutorialspoint.com/exploratory-data-analysis-on-iris-dataset
 # https://www.geeksforgeeks.org/pandas-groupby-and-computing-median/
@@ -65,14 +80,13 @@ def summary_output():
 # petal length and petal width for each of the species variables
 ifds.groupby('Species').agg(['mean', 'median']).describe() # passing a list of recognized strings
 print(ifds.groupby('Species').agg([np.mean, np.median]))
-
-
 '''
+
+# function for plotting a histogram for sepal length
 def sepal_length_hist():
     # https://stackoverflow.com/questions/332289/how-do-you-change-the-size-of-figures-drawn-with-matplotlib
     plt.figure(figsize = (9,9))
-    
-    
+   
     # https://cmdlinetips.com/2019/02/how-to-make-histogram-in-python-with-pandas-and-seaborn/
     sns.distplot(iris_s["SepalLengthCm"],  kde = False, label = "Iris setosa", color = "blue", )
     sns.distplot(iris_vers["SepalLengthCm"],  kde = False, label = "Iris versicolor", color = "orange")
@@ -84,7 +98,6 @@ def sepal_length_hist():
     #plt.savefig("Sepal-lenght.png")
     plt.show()
   
-
 # function for plotting a histogram for sepal width
 def sepal_width_hist():
     plt.figure(figsize = (9,9))
@@ -95,10 +108,10 @@ def sepal_width_hist():
     plt.xlabel("")
     plt.ylabel("Frequency", size = 16)
     plt.legend()
-    #plt.savefig("Sepal-width.png")
+    plt.savefig("Sepal-width.png")
     plt.show()
 
-# function for plotting a histogram for sepal width
+# function for plotting a histogram for petal length
 def petal_length_hist():
     plt.figure(figsize = (9,9))
     sns.distplot(iris_s["PetalLengthCm"],  kde = False, label = "Iris setosa", color = "blue")
@@ -108,9 +121,10 @@ def petal_length_hist():
     plt.xlabel("")
     plt.ylabel("Frequency", size = 16)
     plt.legend()
-    #plt.savefig("Petal-lenght.png")
+    plt.savefig("Petal-lenght.png")
     plt.show()
 
+# function for plotting a histogram for petal width
 def petal_width_hist():
     plt.figure(figsize = (9,9))
     sns.distplot(iris_s["PetalWidthCm"],  kde = False, label = "Iris setosa", color = "blue")
@@ -120,7 +134,7 @@ def petal_width_hist():
     plt.xlabel("")
     plt.ylabel("Frequency", size = 16)
     plt.legend()
-    #plt.savefig("Petal-width.png")
+    plt.savefig("Petal-width.png")
     plt.show()
 
 def histograms():
@@ -163,11 +177,10 @@ def petal_length_width_scat():
     plt.savefig("Petal-length-width.png")
     plt.show()
 
-
 def scatterplots():
     sepal_length_width_scat()
     petal_length_width_scat()
-'''
+
 summary_output()
 
 #histograms()
